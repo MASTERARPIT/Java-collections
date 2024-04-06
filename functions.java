59a524d4bab95f65ebf0729ad55acd01e3dab8f1
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2024-04-06 13:03:34 +0530

    function

--- functions.java
+++ functions.java
@@ -13,30 +13,58 @@ public class functions {
     // }
 
 
-    public static void fact(int n){
+    // FACTORIAL
 
-        int fcat=1;
-        if(n==0){
+    // public static void fact(int n){
 
-            System.out.println("1");
-        }
-        else{
-            for(int i=n;i>=1;i--){
+    //     int fcat=1;
+    //     if(n==0){
 
-                fcat= fcat*i;
-            }
-            System.out.println(fcat);
-        }
-    }
+    //         System.out.println("1");
+    //     }
+    //     else{
+    //         for(int i=n;i>=1;i--){
 
-    public static void main(String[] args){
+    //             fcat= fcat*i;
+    //         }
+    //         System.out.println(fcat);
+    //     }
+    // }
+
+
+// AVERAGE OF N NUMBER USER INPUT
+
+    // public static void average(int n){ 
+    //     System.out.println("average = "+n/10);
+
+    // }
+    // public static void main(String[] args){
+    //     int sum=0;
+    //     Scanner sc = new Scanner(System.in);
+    //     for(int i=0;i<10;i++){
+    //     int n= sc.nextInt();
+    //     sum=sum+n;
+    //     }
+    //     average(sum);
+    // }
+// EVEN ODD  
+    // public static void result(int n){
+    //     if(n%2==0){
+    //         System.out.println("even");
+    //     }
+    //     else{
+    //     System.out.println("odd");
+
+    //     }
+    // }
+
+    public static void result(int n){
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
-        
 
-        fact(n);
+        result(n);
+    }
+    public static void main(String args[]){
         
     }
-    
-    
 }
